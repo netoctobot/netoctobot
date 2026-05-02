a0b439785f9e858eecd83954b9c301896107319f
netoctobot <you@example.come>
2026-05-02 16:42:20 +0300

    push

--- bot/states/main_states.py
+++ bot/states/main_states.py
@@ -2,4 +2,4 @@ from aiogram.fsm.state import StatesGroup, State
 
 class AddBotSG(StatesGroup):
     waiting_for_type = State()  # اختيار النوع
-    waiting_for_token = State() # إرسال التوكن
+    waiting_for_token = State() # إرسال التوكن 
